adf6222f7ea478668290dceaafe36634d34a5b28
IvanaGavranovic <ivanagavranovic@outlook.com>
2020-04-28 13:05:49 +0200

    Drawing lines

--- PR_130_2016/PredmetniZadatak2/PredmetniZadatak2/Handlers/ScreenHandler.cs
+++ PR_130_2016/PredmetniZadatak2/PredmetniZadatak2/Handlers/ScreenHandler.cs
@@ -15,9 +15,9 @@ namespace PredmetniZadatak2.Handlers
         public static ImageDrawing DrawSubstationImage(int indexI, int indexJ, Canvas myCanvas)
         {
             ImageDrawing image = new ImageDrawing();
-            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\circle3.png", UriKind.Relative));
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\circle4.png", UriKind.Relative));
 
-            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 6.8, 3, 3);
+            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 7, 3, 3);
 
             return image;
         }
@@ -25,9 +25,9 @@ namespace PredmetniZadatak2.Handlers
         public static ImageDrawing DrawNodeImage(int indexI, int indexJ, Canvas myCanvas)
         {
             ImageDrawing image = new ImageDrawing();
-            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\circle2.png", UriKind.Relative));
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\circle5.png", UriKind.Relative));
 
-            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 6.8, 3, 3);
+            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 7, 3, 3);
 
             return image;
         }
@@ -37,7 +37,46 @@ namespace PredmetniZadatak2.Handlers
             ImageDrawing image = new ImageDrawing();
             image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\circle1.png", UriKind.Relative));
 
-            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 6.8, 3, 3);
+            image.Rect = new Rect((indexJ / 1000.0) * myCanvas.Width * 4.7, (indexI / 1000.0) * myCanvas.Height * 7, 3, 3);
+
+            return image;
+        }
+
+        public static ImageDrawing DrawRightHorizontalLine(double x1, double y1, double x2, double y2)
+        {
+            ImageDrawing image = new ImageDrawing();
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\horizontalLine.png", UriKind.Relative));
+
+            image.Rect = new Rect(x1 + 1.5, y1 + 0.5, x2 - x1, 2);
+
+            return image;
+        }
+
+        public static ImageDrawing DrawLeftHorizontalLine(double x1, double y1, double x2, double y2)
+        {
+            ImageDrawing image = new ImageDrawing();
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\horizontalLine.png", UriKind.Relative));
+
+            image.Rect = new Rect(x2 + 1.5, y1 + 0.5, x1 - x2, 2);
+
+            return image;
+        }
+
+        public static ImageDrawing DrawDownVerticalLine(double x1, double y1, double x2, double y2)
+        {
+            ImageDrawing image = new ImageDrawing();
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\verticalLine.png", UriKind.Relative));
+
+            image.Rect = new Rect(x1 + 0.5, y1 + 1.5, 2, y2 - y1);
+
+            return image;
+        }
+
+        public static ImageDrawing DrawUpVerticalLine(double x1, double y1, double x2, double y2)
+        {
+            ImageDrawing image = new ImageDrawing();
+            image.ImageSource = new BitmapImage(new Uri(@"..\..\Images\verticalLine.png", UriKind.Relative));
+            image.Rect = new Rect(x1 + 0.5, y2 + 1.5, 2, y1 - y2);
 
             return image;
         }
--- PR_130_2016/PredmetniZadatak2/PredmetniZadatak2/MainWindow.xaml.cs
+++ PR_130_2016/PredmetniZadatak2/PredmetniZadatak2/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace PredmetniZadatak2
     {
         public UInt64[,] grid = new UInt64[1000, 1000];
 
+        private List<Tuple<UInt64, UInt64>> drawnLines = new List<Tuple<ulong, ulong>>();
+
         private NetworkModel networkModel = new NetworkModel();
        
         private DrawingImage drawingImage;
@@ -38,6 +40,184 @@ namespace PredmetniZadatak2
 
             grid = GridHandler.MakeGrid();
             networkModel = GridHandler.LoadNetworkModel(networkModel, grid, drawingGroup, myCanvas, out grid);
+
+            DrawLines();
+        }
+
+        public void DrawLines()
+        {
+            for (int i = 0; i < networkModel.Lines.Count; i++)
+            {
+                UInt64 idStart = networkModel.Lines[i].FirstEnd;
+                UInt64 idStop = networkModel.Lines[i].SecondEnd;
+
+                // provera da li oba ID-a uopste postoje
+                if (!GridHandler.Entities.ContainsKey(idStart) || !GridHandler.Entities.ContainsKey(idStop))
+                {
+                    continue;
+                }
+
+                // provera da li je vec nacrtana ta linija -> putanja A-B je isto sta i B-A, tako da je ne treba crtati
+                if (i == 0)
+                {
+                    drawnLines.Add(new Tuple<ulong, ulong>(idStart, idStop));
+                }
+                else if (drawnLines.Contains(new Tuple<ulong, ulong>(idStart, idStop)) || drawnLines.Contains(new Tuple<ulong, ulong>(idStop, idStart)))
+                {
+                    continue;
+                }
+                else
+                {
+                    drawnLines.Add(new Tuple<ulong, ulong>(idStart, idStop));
+                }
+
+                // indeks prvog cvora
+                int rowStart = GridHandler.Entities[idStart].Row;
+                int columnStart = GridHandler.Entities[idStart].Column;
+
+                // indeks drugog cvora
+                int rowStop = GridHandler.Entities[idStop].Row;
+                int columnStop = GridHandler.Entities[idStop].Column;
+
+                double x1 = (columnStart / 1000.0) * myCanvas.Width * 4.7;
+                double y1 = (rowStart / 1000.0) * myCanvas.Height * 7;
+                double x2 = (columnStop / 1000.0) * myCanvas.Width * 4.7;
+                double y2 = (rowStop / 1000.0) * myCanvas.Height * 7;
+
+                // HORIZONTALA
+                if (rowStart == rowStop)
+                {
+                    // u desno
+                    if (columnStart < columnStop)
+                    {
+                        ImageDrawing line = ScreenHandler.DrawRightHorizontalLine(x1, y1, x2, y2);
+                        drawingGroup.Children.Add(line);
+                    }
+                    // u levo
+                    else
+                    {
+                        ImageDrawing line = ScreenHandler.DrawLeftHorizontalLine(x1, y1, x2, y2);
+                        drawingGroup.Children.Add(line);
+                    }
+                }
+
+                // VERTIKALA
+                if (columnStart == columnStop)
+                {
+                    // na dole
+                    if (rowStart < rowStop)
+                    {
+                        ImageDrawing line = ScreenHandler.DrawDownVerticalLine(x1, y1, x2, y2);
+                        drawingGroup.Children.Add(line);
+                    }
+                    // na gore
+                    else
+                    {
+                        ImageDrawing line = ScreenHandler.DrawUpVerticalLine(x1, y1, x2, y2);
+                        drawingGroup.Children.Add(line);
+                    }
+                }
+
+                // GORE LEVO
+                if (rowStart > rowStop && columnStart > columnStop)
+                {
+                    if (i % 2 == 0)
+                    {
+                        // idem na gore, ne menja se x1 koordinata
+                        ImageDrawing lineUp = ScreenHandler.DrawUpVerticalLine(x1, y1, x1, y2);
+                        drawingGroup.Children.Add(lineUp);
+
+                        // idem na levo, ne menja se y2 koordinata
+                        ImageDrawing lineLeft = ScreenHandler.DrawLeftHorizontalLine(x1, y2, x2, y2);
+                        drawingGroup.Children.Add(lineLeft);
+                    }
+                    else
+                    {
+                        // idem prvo na levo, ne menja se y1 koordinata
+                        ImageDrawing lineLeft = ScreenHandler.DrawLeftHorizontalLine(x1, y1, x2, y1);
+                        drawingGroup.Children.Add(lineLeft);
+
+                        // idem na gore, ne menja se x2 koordinata
+                        ImageDrawing lineUp = ScreenHandler.DrawUpVerticalLine(x2, y1, x2, y2);
+                        drawingGroup.Children.Add(lineUp);
+                    }
+                }
+
+                // GORE DESNO
+                else if (rowStart > rowStop && columnStart < columnStop)
+                {
+                    if (i % 2 == 0)
+                    {
+                        // idem na gore, ne menja se x1 koordinata
+                        ImageDrawing lineUp = ScreenHandler.DrawUpVerticalLine(x1, y1, x1, y2);
+                        drawingGroup.Children.Add(lineUp);
+
+                        // idem u desno, ne menja se y2 koordinata
+                        ImageDrawing lineRight = ScreenHandler.DrawRightHorizontalLine(x1, y2, x2, y2);
+                        drawingGroup.Children.Add(lineRight);
+                    }
+                    else
+                    {
+                        // idem na desno, ne menja se y1 koordinata
+                        ImageDrawing lineRight = ScreenHandler.DrawRightHorizontalLine(x1, y1, x2, y1);
+                        drawingGroup.Children.Add(lineRight);
+
+                        // idem na gore, ne menja se x2 koordinata
+                        ImageDrawing lineUp = ScreenHandler.DrawUpVerticalLine(x2, y1, x2, y2);
+                        drawingGroup.Children.Add(lineUp);
+                    }
+                }
+
+                // DOLE LEVO
+                else if (rowStart < rowStop && columnStart > columnStop)
+                {
+                    if (i % 2 == 0)
+                    {
+                        // idem na dole, ne menja se x1 koordinata
+                        ImageDrawing lineDown = ScreenHandler.DrawDownVerticalLine(x1, y1, x1, y2);
+                        drawingGroup.Children.Add(lineDown);
+
+                        // idem na levo, ne menja se y2 koordinata
+                        ImageDrawing lineLeft = ScreenHandler.DrawLeftHorizontalLine(x1, y2, x2, y2);
+                        drawingGroup.Children.Add(lineLeft);
+                    }
+                    else
+                    {
+                        // idem prvo na levo, ne menja se y1 koordinata
+                        ImageDrawing lineLeft = ScreenHandler.DrawLeftHorizontalLine(x1, y1, x2, y1);
+                        drawingGroup.Children.Add(lineLeft);
+
+                        // idem na dole, ne menja se x2 koordinata
+                        ImageDrawing lineDown = ScreenHandler.DrawDownVerticalLine(x2, y1, x2, y2);
+                        drawingGroup.Children.Add(lineDown);
+                    }
+                }
+
+                // DOLE DESNO
+                else if (rowStart < rowStop && columnStart < columnStop)
+                {
+                    if (i % 2 == 0)
+                    {
+                        // idem na dole, ne menja se x1 koordinata
+                        ImageDrawing lineDown = ScreenHandler.DrawDownVerticalLine(x1, y1, x1, y2);
+                        drawingGroup.Children.Add(lineDown);
+
+                        // idem u desno, ne menja se y2 koordinata
+                        ImageDrawing lineRight = ScreenHandler.DrawRightHorizontalLine(x1, y2, x2, y2);
+                        drawingGroup.Children.Add(lineRight);
+                    }
+                    else
+                    {
+                        // idem na desno, ne menja se y1 koordinata
+                        ImageDrawing lineRight = ScreenHandler.DrawRightHorizontalLine(x1, y1, x2, y1);
+                        drawingGroup.Children.Add(lineRight);
+
+                        // idem na dole, ne menja se x2 koordinata
+                        ImageDrawing lineDown = ScreenHandler.DrawDownVerticalLine(x2, y1, x2, y2);
+                        drawingGroup.Children.Add(lineDown);
+                    }
+                }
+            }
         }
     }
 }
